bd96beed1d51797fc9e74077d64714696576627d
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2026-07-05 11:12:57 +0200

    Add initial player implementation

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,3 +1,10 @@
 package controller
 
-object Controller
+object Controller:
+  def run(): Unit = ???
+    // TO DO chiamata metodo alla view per ottenere balance e nome del giocatore
+    // TO DO creare istanza della partita passando la lista dei giocatori definiti dall'utente che avvia la partita
+
+@main def startApplication(): Unit =
+  import Controller.*
+  run()
---
+++ src/test/scala/PlayerTest.scala
@@ -0,0 +1,45 @@
+import model.Model.PlayerModule.*
+import org.scalatest.matchers.should.Matchers.*
+import org.scalatest.funsuite.AnyFunSuite
+
+
+class PlayerTest extends AnyFunSuite:
+
+  val startingAmount = 50
+  val name = "gigi"
+  val player: Player = Player(name, startingAmount)
+
+  test("starting player's state should be always Active"){
+    player.state shouldBe PlayerState.Active
+  }
+
+  test("starting player's balance should be the exact amount defined by the user"){
+   // player.balance.convertToCurrency() shouldBe startingAmount
+  }
+
+  test("player's name should be the one decided by the user"){
+    player.name shouldBe name
+  }
+
+  test("player's state change should work as expected"){
+    player.bust()
+    player.state shouldBe PlayerState.Busted
+    player.stand()
+    player.state shouldBe PlayerState.Standing
+    player.leaveTable()
+    player.state shouldBe PlayerState.LeftGame
+  }
+
+  //TO DO modificare quando le fiches sono implementate
+  test("the withdrawn method should work has expected when the bet amount is valid"){
+    val bet = 45
+    val invalidBet = 55
+    player.withdrawn(bet) shouldBe true
+    player.balance.sum shouldBe startingAmount - bet
+  }
+
+  test("the withdrawn method should work has expected when the bet amount is not valid"){
+    val invalidBet = 55
+    player.withdrawn(invalidBet) should not be true
+    player.balance.sum shouldBe startingAmount
+  }
