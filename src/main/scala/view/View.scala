4637758879c2d307212167d7c39325127e337003
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-07-05 16:43:50 +0200

    added method to get user Id from standard input

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -5,21 +5,36 @@ import cats.effect.std.Console
 
 object View:
 
+  /** Interactively prompts the user to enter their ID.
+   *
+   * This method prints a welcome message and an ID prompt to the terminal,
+   * then suspends the execution until the user provides an input via standard input.
+   *
+   * @param console the contextual [[cats.effect.std.Console]] instance used to perform
+   *                pure and testable I/O operations.
+   * @return a [[cats.effect.IO]] encapsulating the computation that yields the player's ID
+   *         as a [[String]] upon evaluation.
+   */
+  def getPlayerID(using console: Console[IO]): IO[String] =
+    for
+      _        <- console.println("Welcome to the game! Please insert your ID: ")
+      playerID <- console.readLine
+    yield playerID
+
   /** Interactively prompts the user to enter their initial playing balance.
    *
    * This method prints a prompt to the terminal and reads the user's input.
    * If the input is not a valid positive number, it prints an error message
    * and recursively prompts the user again until a valid balance is provided.
    *
-   * It wraps the terminal I/O side effects inside a [[cats.effect.IO]] container
-   * to maintain referential transparency.
+   * @param console the contextual [[cats.effect.std.Console]] instance used to perform
+   *                pure and testable I/O operations.
    *
-   * @return an [[cats.effect.IO]] encapsulating the computation
-   *         that yields the initial balance as a [[Int]].
+   * @return an [[cats.effect.IO]] encapsulating the computation that yields the
+   *         initial balance as a [[Int]].
    */
   def getInitialBalance(using console: Console[IO]): IO[Int] =
     for
-      _       <- console.println("Welcome to the game!")
       _       <- console.println("Please insert your initial balance: ")
       input   <- console.readLine
       balance <- input.toIntOption match
--- src/test/scala/view/ViewTest.scala
+++ src/test/scala/view/ViewTest.scala
@@ -12,36 +12,29 @@ import java.nio.charset.Charset
 
 class ViewTest extends AnyFunSuite:
 
+  val expectedPlayerID = "mario123"
   val expectedBalance: Int = 100
 
+  def mockConsoleWith(readLineBehavior: IO[String]): Console[IO] = new Console[IO]:
+    override def readLine: IO[String] = readLineBehavior
+    override def readLineWithCharset(charset: Charset): IO[String] = readLine
+    override def print[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
+    override def println[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
+    override def error[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
+    override def errorln[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
+
+  test("The ID of the player should equal what is simulated in standard input"):
+    given Console[IO] = mockConsoleWith(IO.pure(expectedPlayerID))
+    val actualPlayerID: String = getPlayerID.unsafeRunSync()
+    actualPlayerID shouldEqual expectedPlayerID
+
   test("The initial balance of the player should equal what is simulated in standard input"):
-
-    given mockConsole: Console[IO] with
-      override def readLine: IO[String] = IO.pure(expectedBalance.toString)
-      override def readLineWithCharset(charset: Charset): IO[String] = readLine
-      override def print[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def println[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def error[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def errorln[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-
-    val method: IO[Int] = getInitialBalance
-    val actualBalance: Int = method.unsafeRunSync()
-
+    given Console[IO] = mockConsoleWith(IO.pure(expectedBalance.toString))
+    val actualBalance: Int = getInitialBalance.unsafeRunSync()
     actualBalance shouldEqual expectedBalance
 
   test("The view should retry until a valid positive integer is provided"):
-
     val simulatedInputs = Iterator("error", "-50", expectedBalance.toString)
-
-    given mockConsole: Console[IO] with
-      override def readLine: IO[String] = IO(simulatedInputs.next())
-      override def readLineWithCharset(charset: Charset): IO[String] = readLine
-      override def print[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def println[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def error[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-      override def errorln[A](a: A)(using S: Show[A]): IO[Unit] = IO.unit
-
-    val method: IO[Int] = getInitialBalance
-    val actualBalance: Int = method.unsafeRunSync()
-
+    given Console[IO] = mockConsoleWith(IO(simulatedInputs.next()))// We use IO(...) instead of IO.pure to suspend the evaluation of iterator.next() until there is a new execution of readLine()
+    val actualBalance: Int = getInitialBalance.unsafeRunSync()
     actualBalance shouldEqual expectedBalance
