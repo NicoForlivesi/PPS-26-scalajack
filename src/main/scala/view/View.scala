c8bf1adbf873d35f0df2710c31486c875fedd097
Anna Malagoli <anna.malagoli2@studio.unibo.it>
2026-07-06 10:52:28 +0200

    Implemented improvement in View to reuse common code

--- src/main/scala/view/View.scala
+++ src/main/scala/view/View.scala
@@ -6,6 +6,45 @@ import model.PlayerModule.Player
 
 object View:
 
+  /** Validates a raw string input by attempting to parse it into a value of type `A`
+   * and then checking it against a given predicate.
+   *
+   * @param input     the raw user input as a string
+   * @param parser    a function that attempts to convert the string into a value of type `A`
+   *                  (returns `None` if parsing fails)
+   * @param predicate a condition that the parsed value must satisfy
+   * @tparam A the target type of the parsed and validated value
+   * @return Some(value) if parsing succeeds and the predicate condition matches, None otherwise
+   */
+  private def isInputValid[A](input: String, parser: String => Option[A], predicate: A => Boolean): Option[A] =
+    parser(input).filter(predicate)
+
+  /** Repeatedly prompts the user until a valid input is provided.
+
+   * @param prompt    the message displayed to request input from the user
+   * @param error     the message displayed when the input is invalid
+   * @param parser    function used to parse the input string into a value of type `A`
+   * @param predicate condition that the parsed value must satisfy
+   * @tparam A the type of the validated result
+   * @return an IO effect producing a valid value of type `A`
+   */
+  private def askUntilValidPrompt[A](prompt: String,
+                                     error: String,
+                                     parser: String => Option[A],
+                                     predicate: A => Boolean)(using console: Console[IO]): IO[A] =
+    for
+      _ <- console.println(prompt)
+      input <- console.readLine
+      value <- isInputValid(input, parser, predicate) match
+        case Some(v) => IO.pure(v)
+        case None =>
+          for
+            _          <- console.println(error)
+            retryValue <- askUntilValidPrompt(prompt, error, parser, predicate)
+          yield retryValue
+    yield value
+
+
   /** Interactively prompts the user to enter the number of players in the match.
    *
    * @param console the contextual [[cats.effect.std.Console]] instance used to perform
@@ -15,18 +54,13 @@ object View:
    */
   def getNumPlayers(using console: Console[IO]): IO[Int] =
     for
-      _ <- console.println("Please insert the number of players in the game:")
-      input <- console.readLine
-      numPlayers <- input.toIntOption match
-        case Some(count) if count > 0 =>
-          for
-            _ <- console.println(s"Perfect! The game will start with $count players.")
-          yield count
-        case _ =>
-          for
-            _          <- console.println("Sorry, the number of players must be a valid number greater than 0")
-            retryCount <- getNumPlayers
-          yield retryCount
+      numPlayers <- askUntilValidPrompt(
+        prompt = "Please insert the number of players in the game:",
+        error = "Sorry, the number of players must be a valid number greater than 0",
+        parser = _.toIntOption,
+        predicate = _ > 0
+      )
+      _ <- console.println(s"Perfect! The game will start with $numPlayers players.")
     yield numPlayers
 
   /** Interactively prompts the user to enter their ID.
@@ -42,7 +76,7 @@ object View:
   def getPlayerID(using console: Console[IO]): IO[String] =
     for
       _        <- console.println("Welcome to the game!")
-      _        <- console.println("Please insert your ID below:")
+      _        <- console.println("Please insert your ID below: ")
       playerID <- console.readLine
       _        <- console.println(s"Your ID $playerID has been correctly added!")
     yield playerID
@@ -61,18 +95,13 @@ object View:
    */
   def getInitialBalance(using console: Console[IO]): IO[Int] =
     for
-      _       <- console.println("Please insert your initial balance in € below: ")
-      input   <- console.readLine
-      balance <- input.toIntOption match
-        case Some(amount) if amount > 0 =>
-          for
-            _ <- console.println(s"Your balance of €$amount has been correctly added! Now it will be converted in fiches.")
-          yield amount
-        case _ =>
-          for
-            _            <- console.println("Sorry, your input is not valid!")
-            retryBalance <- getInitialBalance
-          yield retryBalance
+    balance <- askUntilValidPrompt(
+        prompt = "Please insert your initial balance in € below: ",
+        error = "Sorry, your input is not valid!",
+        parser = _.toIntOption,
+        predicate = _ > 0
+      )
+      _ <- console.println(s"Your balance of €$balance has been correctly added! Now it will be converted in fiches.")
     yield balance
 
   /** Interactively prompts a player to enter their bet for the upcoming hand via the console.
@@ -85,19 +114,17 @@ object View:
    * @return A [[cats.effect.IO]] that, when evaluated, contains the valid
    *         bet amount.
    */
-  def getBet(player: Player)(using console: Console[IO]): IO[Int] =
+  def getBet(player: Player)(using console: Console[IO]): IO[Int] = {
     for
-      _        <- console.println(s"Your actual balance is ${player.balance.sum} fiches.")
-      _        <- console.println("Please insert your bet for the upcoming hand!")
-      bet      <- console.readLine
-      validBet <- bet.toIntOption match
-        case Some(amount) if amount > 0 && amount <= player.balance.sum =>
-          for
-            _ <- console.println(s"Your bet of $amount fiches has been correctly added!")
-          yield amount
-        case _ =>
-          for
-            _        <- console.println("Sorry, your input is not valid!")
-            retryBet <- getBet(player)
-          yield retryBet
-    yield validBet
+      _        <- console.println(s"[${player.name}]Your actual balance is ${player.balance.sum} fiches.")
+      _        <- console.println(s"[${player.name}]Please insert your bet for the upcoming hand!")
+      betAmount <- askUntilValidPrompt(
+        prompt = s"[${player.name}]Please insert your bet for the upcoming hand!",
+        error = "Sorry, your input is not valid!",
+        parser = _.toIntOption,
+        predicate = (bet: Int) => bet > 0 && bet <= player.balance.sum
+      )
+      _ <- console.println(s"Your bet of $betAmount fiches has been correctly added!")
+    yield betAmount
+
+  }
