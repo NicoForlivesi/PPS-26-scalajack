e2ca326797392213bd451d1fed17f8f53abae753
NicoForlivesi <nicholas.forlivesi@studio.unibo.it>
2026-07-10 21:26:13 +0200

    ControllerTest: added some 'TODO'

--- src/test/scala/controller/ControllerTest.scala
+++ src/test/scala/controller/ControllerTest.scala
@@ -207,6 +207,7 @@ class ControllerTest extends AnyFunSuite with BeforeAndAfterEach:
     handleHands(testGame).unsafeRunSync()
     testGame.isCutCardInDeck shouldBe false
 
+  // TODO questo si inloppa all'infinito, commentarlo momentaneamente per poter testare quelli dopo
   test("handleHands should terminate immediately if there are no players left"):
     game.removePlayer(player1)
     game.removePlayer(player2)
@@ -215,6 +216,7 @@ class ControllerTest extends AnyFunSuite with BeforeAndAfterEach:
     handleHands(game).unsafeRunSync()
     game.isCutCardInDeck shouldBe initialCutCardState
 
+  // TODO questo non passa
   test("Method endHand should correctly remove from the game all the players that want to leave"):
     val simulatedInputs = Iterator("N", "Y")
     given mockConsole: Console[IO] = mockConsoleWith(() => simulatedInputs.next())
